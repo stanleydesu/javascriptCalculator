943a207bde3c1c2c847749d5ad1204ea9e868da5
Stanley Su <stanleysu00@gmail.com>
2017-08-16 22:24:20 +1000

    added function to only add valid inputs to expression

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+function handleInput(arr, input) {
+	arr.push(input);
+	// expression must start with number, initialise with true
+	let isNumber = true;
+	for (let i = 0, len = arr.length; i < len; ++i) {
+		if (isNumber) {
+			if (typeof(arr[i]) !== 'number') {
+				arr.pop();
+			}
+			isNumber = false;
+		} else {
+			if (typeof(arr[i]) === 'number') {
+				arr.pop();
+			}
+			isNumber = true;				
+		}
+	}
+}
+
